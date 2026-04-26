b659fa218f02b275daffe99838299c05921a60c6
Dezlost <dezt.root@gmail.com>
2026-04-26 12:51:49 -0400

    feat(configdashboard)"Logre agregar una dashboard e ire trabajando en ella)"

---
+++ lua/snacks/configdashboard.lua
@@ -0,0 +1,94 @@
+return {
+  enabled = true,
+  ---@class snacks.dashboard.Config
+  ---@field enabled? boolean
+  ---@field sections snacks.dashboard.Section
+  ---@field formats table<string, snacks.dashboard.Text|fun(item:snacks.dashboard.Item, ctx:snacks.dashboard.Format.ctx):snacks.dashboard.Text>
+  width = 80,
+  row = nil,                                                                   -- dashboard position. nil for center
+  col = nil,                                                                   -- dashboard position. nil for center
+  pane_gap = 4,                                                                -- empty columns between vertical panes
+  autokeys = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", -- autokey sequence
+  -- These settings are used by some built-in sections
+  preset = {
+    -- Defaults to a picker that supports `fzf-lua`, `telescope.nvim` and `mini.pick`
+    ---@type fun(cmd:string, opts:table)|nil
+    pick = nil,
+    -- Used by the `keys` section to show keymaps.
+    -- Set your custom keymaps here.
+    -- When using a function, the `items` argument are the default keymaps.
+    ---@type snacks.dashboard.Item[]
+    keys = {
+      { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+      { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+      { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+      { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+      { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+      { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+      { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
+      { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+    },
+    -- Used by the `header` section
+    header = [[
+██████╗ ██████╗  █████╗ ██╗██╗  ██╗ ██████╗
+██╔══██╗██╔══██╗██╔══██╗██║██║ ██╔╝██╔═══██╗
+██║  ██║██████╔╝███████║██║█████╔╝ ██║   ██║
+██║  ██║██╔══██╗██╔══██║██║██╔═██╗ ██║   ██║
+██████╔╝██║  ██║██║  ██║██║██║  ██╗╚██████╔╝
+╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝╚═╝  ╚═╝ ╚═════╝]],
+  },
+  -- item field formatters
+  formats = {
+    icon = function(item)
+      if item.file and item.icon == "file" or item.icon == "directory" then
+        return Snacks.dashboard.icon(item.file, item.icon)
+      end
+      return { item.icon, width = 2, hl = "icon" }
+    end,
+    footer = { "%s", align = "center" },
+    header = { "%s", align = "center" },
+    file = function(item, ctx)
+      local fname = vim.fn.fnamemodify(item.file, ":~")
+      fname = ctx.width and #fname > ctx.width and vim.fn.pathshorten(fname) or fname
+      if #fname > ctx.width then
+        local dir = vim.fn.fnamemodify(fname, ":h")
+        local file = vim.fn.fnamemodify(fname, ":t")
+        if dir and file then
+          file = file:sub(-(ctx.width - #dir - 2))
+          fname = dir .. "/…" .. file
+        end
+      end
+      local dir, file = fname:match("^(.*)/(.+)$")
+      return dir and { { dir .. "/", hl = "dir" }, { file, hl = "file" } } or { { fname, hl = "file" } }
+    end,
+  },
+  sections = {
+    { section = "header" },
+    {
+      pane = 2,
+      section = "terminal",
+      cmd = "pokemon-colorscripts -n umbreon -s --no-title; sleep .1",
+      height = 15,
+      indent = 4,
+      random = 10,
+    },
+    { section = "keys", gap = 1, padding = 1 },
+    { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+    { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+    {
+      pane = 2,
+      icon = " ",
+      title = "Git Status",
+      section = "terminal",
+      enabled = function()
+        return Snacks.git.get_root() ~= nil
+      end,
+      cmd = "git status --short --branch --renames",
+      height = 5,
+      padding = 1,
+      ttl = 5 * 60,
+      indent = 3,
+    },
+    { section = "startup" },
+  },
+}
