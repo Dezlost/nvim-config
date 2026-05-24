71418352209d02bcd9995f575cca845d81a409c2
Dezlost <dezt.root@gmail.com>
2026-05-23 22:45:20 -0400

    fix(lsp.lua)"si algo se rompe fue por esto"

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,6 +1,33 @@
 return {
-    "neovim/nvim-lspconfig",
-    config = function()
-        vim.lsp.enable({"ts_ls","cssls","tailwindcss","html","pyright","lua_ls","jsonls","prisma"})
-    end
+	"neovim/nvim-lspconfig",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"saghen/blink.cmp",
+	},
+	config = function()
+		local lspconfig = require("lspconfig")
+		local mason_lspconfig = require("mason-lspconfig")
+		local blink = require("blink.cmp")
+
+		vim.api.nvim_create_autocmd("LspAttach", {
+			callback = function(event)
+				local opts = { buffer = event.buf }
+				vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+				vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+				vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+			end,
+		})
+
+		local capabilities = blink.get_lsp_capabilities()
+
+		mason_lspconfig.setup({
+			handlers = {
+				function(server_name)
+					lspconfig[server_name].setup({
+						capabilities = capabilities,
+					})
+				end,
+			},
+		})
+	end,
 }
